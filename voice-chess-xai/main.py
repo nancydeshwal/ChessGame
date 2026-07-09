99fb78b86979484eafad9766014372959497e45f
nancydeshwal <nancydeshwal29@gmail.com>
2026-07-09 20:30:03 +0530

    Delete voice-chess-xai directory

--- voice-chess-xai/main.py
+++
@@ -1,8 +0,0 @@
-import chess
-
-print(chess.piece_name
-      (chess.PAWN))  # Output: pawn
-print(chess.PieceType(chess.KNIGHT))  # Output: 2
-
-piece = chess.Piece(chess.KNIGHT, chess.WHITE)
-print(piece.piece_type)  # Output: 2
